690001ef8ea62154f250ccb78170e201a9339037
BlUnGoGo <jsangay@etsinf.upv.es>
2024-11-21 13:00:32 +0100

    Se crea el proyecto

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_quatroEnRalla_FILE C:/Users/jaume/OneDrive/Escritorio/UNI/3r/IHM/quatroEnRalla/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/quatroEnRalla.exe)
+set(__QT_DEPLOY_TARGET_quatroEnRalla_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_quatroEnRalla_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
