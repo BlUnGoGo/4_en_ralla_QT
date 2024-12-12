ed13b1d4a8007a2e796d3bb00ccde56b9c6713cd
Jaume Sancho Gayà <jsangay@etsinf.upv.es>
2024-12-12 11:48:38 +0100

    Integración de la toolBar
    Boceto inicial de la toolbar para empezar a trabajar en ella

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,40 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "gameboard.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Crear acciones para la barra de herramientas
+    QAction *gameboardAction = new QAction("Jugar", this);
+    //QWidged *Label = new QLabel("4 en ralla");
+
+
+    // Agregar acciones a la barra de herramientas
+    ui->toolBar->addAction(gameboardAction);
+    ui->toolBar->addWidget(ui->label);
+
+    // Conectar acciones con slots
+    connect(gameboardAction, &QAction::triggered, this, &MainWindow::showGameboardWidget);
+
+
+    //Mostrar inicialmete el gameboard
+    //showGameboardWidget();
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::showGameboardWidget() {
+    if (currentWidget) {
+        delete currentWidget;
+    }
+    currentWidget = new GameBoard(this);
+    setCentralWidget(currentWidget);
 }
